f57d03eb5e083a150bff34c8da5b27320e8ec05d
Christopher Chen <christopher.chen1995@gmail.com>
2017-07-22 16:47:51 -0700

    Remove unnecessary post deploy step

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,4 +1,2 @@
 # https://brettdewoody.com/deploying-a-heroku-app-from-a-subdirectory/
 git push heroku `git subtree split --prefix webapp heroku`:master --force
-# ensure card similarity models get built on heroku
-heroku run python3 build_all.py

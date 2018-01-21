1502115d92cc8db74fcdc2275567852a49c70ebc
Christopher Chen <christopher.chen1995@gmail.com>
2018-01-20 21:50:38 -0800

    Update for RIX

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,2 +1,4 @@
+#1/bin/bash
 # https://brettdewoody.com/deploying-a-heroku-app-from-a-subdirectory/
-git push heroku `git subtree split --prefix webapp heroku`:master --force
+#git push heroku `git subtree split --prefix webapp heroku`:master --force
+git subtree push --prefix=webapp heroku master

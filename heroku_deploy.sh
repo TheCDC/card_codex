2578f99b2c5b6491d1affb1ba035f504e49e3221
Christopher Chen <christopher.chen1995@gmail.com>
2018-01-21 09:49:42 -0800

    Fix deploy script

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,4 +1,4 @@
 #1/bin/bash
 # https://brettdewoody.com/deploying-a-heroku-app-from-a-subdirectory/
-#git push heroku `git subtree split --prefix webapp heroku`:master --force
-git subtree push --prefix=webapp heroku master
+git push heroku `git subtree split --prefix webapp heroku`:master --force
+#git subtree push --prefix=webapp heroku master

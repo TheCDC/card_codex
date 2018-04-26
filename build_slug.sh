f4e881e6b52ed7429df6d786c3a05d88d64bdf46
Christopher Chen <christopher.chen1995@gmail.com>
2018-04-25 19:27:14 -0700

    Re add stuff that was deleted? Also build and upload script. Deployed app doesn't work yet due to python dist not being portable.

--- build_slug.sh
+++ build_slug.sh
@@ -1,4 +1,8 @@
-mkdir slug
-cp -R webapp/* slug
-tar czfv slug.tgz ./slug
-rm -rf slug/*
+mkdir app
+cp -R webapp/* app
+rm slug.tgz
+tar czfv slug.tgz ./app
+# tar --strip-components 1 -xvf slug.tgz
+# https://stackoverflow.com/questions/939982/how-do-i-tar-a-directory-of-files-and-folders-without-including-the-directory-it/39530409#39530409
+# find webapp -printf "%P\n" -type f -o -type l -o -type d | tar -czf slug.tgz --no-recursion -C webapp -T -
+rm -rf app
---
+++ upload_slug.sh
@@ -0,0 +1,32 @@
+#
+# https://devcenter.heroku.com/articles/platform-api-deploying-slugs#create-the-slug
+CHECKSUM=`shasum --algorithm 256 slug.tgz | python3 -c "import sys;[print(i.split(' ')[0]) for i in sys.stdin]"`
+echo "Publish to platform"
+RESPONSE=`curl -X POST \
+-H 'Content-Type: application/json' \
+-H 'Accept: application/vnd.heroku+json; version=3' \
+-d "{\"checksum\":\"$CHECKSUM\", \"process_types\":{\"web\":\"env/bin/python3 app.py\"}}" \
+-n https://api.heroku.com/apps/card-codex-dev/slugs`
+echo $RESPONSE
+URL=`echo $RESPONSE | python -c "import json;import sys;print(json.load(sys.stdin)['blob']['url'])"`
+ID=`echo $RESPONSE | python -c "import json;import sys;print(json.load(sys.stdin)['id'])"`
+echo "URL=$URL"
+echo "ID=$ID"
+
+
+echo "Upload slug"
+curl -X PUT \
+-H "Content-Type:" \
+--data-binary @slug.tgz \
+$URL
+
+
+echo "Release slug"
+SLUGJSON="{\"slug\":\"$ID\"}"
+echo "SLUGJSON=$SLUGJSON"
+curl -X POST \
+-H "Accept: application/vnd.heroku+json; version=3" \
+-H "Content-Type: application/json" \
+-d $SLUGJSON \
+-n https://api.heroku.com/apps/card-codex-dev/releases
+echo "========== Done =========="
